56c2a0e8bd1062e69652fbce47701a6166ab507b
Cris Mihalache <me@f3rno.com>
2019-07-24 23:57:53 +0700

    (feature) ao adapter [WIP]

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const syncTradesRange = require('./lib/models/trade/sync_range')
 const syncCandlesRange = require('./lib/models/candle/sync_range')
 const auditCandleGaps = require('./lib/models/candle/audit_gaps')
+const AOAdapter = require('./lib/ao_adapter')
 
 module.exports = {
+  AOAdapter,
+
   schema: {
     Trade: {
       schemaExchangeData: {
